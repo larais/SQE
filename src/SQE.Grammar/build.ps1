856a735ae86987ad3895577d67c943727d362b14
David Morais Ferreira <david.moraisferreira@gmail.com>
2017-11-09 23:01:21 +0100

    build.ps1 is now called in the Pre-Build Event

--- src/SQE.Grammar/build.ps1
+++ src/SQE.Grammar/build.ps1
@@ -1,7 +1,19 @@
-$AntlrJar = "./antlr-4.7-complete.jar"
+param(
+	[String]$AntlrJar="./antlr-4.7-complete.jar",
+	[String]$Grammar="SQE.g4",
+	[String]$Package="SQE",
+	[String]$Output="../SQE.CSharp/Generated"
+)
+
 if (-Not (Test-Path($AntlrJar)))
 {
 	Invoke-WebRequest http://www.antlr.org/download/$AntlrJar -OutFile $AntlrJar
 }
 
-java -jar $AntlrJar -Dlanguage=CSharp SQE.g4 -visitor -no-listener -package SQE -o "../SQE.CSharp/Generated"
+try
+{
+	java -jar $AntlrJar -Dlanguage=CSharp $Grammar -visitor -no-listener -package $Package -o $Output
+}
+catch {
+	Write-Error "Could not generate the C# files, please verify that you have java installed on your machine."
+}
