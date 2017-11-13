e3948d2714ba648ead6013ebafdea362a62eb40c
Sebastian Lang <admin@sebastian-lang.eu>
2017-11-13 23:01:47 +0100

    Unit tests

---
+++ src/SQE.CSharp/ErrorListener.cs
@@ -0,0 +1,28 @@
+﻿using Antlr4.Runtime;
+using System;
+using System.IO;
+
+namespace SQE.CSharp
+{
+    public class ErrorListener : BaseErrorListener
+    {
+        public event EventHandler<ErrorEventArgs> Error;
+
+        public override void SyntaxError(TextWriter output, IRecognizer recognizer, IToken offendingSymbol, int line, int charPositionInLine, string msg, RecognitionException e)
+        {
+            if (Error != null)
+            {
+                Error(this, new ErrorEventArgs { Error = e != null ? e : new Exception("SyntaxError" + e) });
+            }
+            else
+            {
+                throw new Exception("SyntaxError" + e);
+            }
+        }
+    }
+
+    public class ErrorEventArgs : EventArgs
+    {
+        public Exception Error { get; set; }
+    }
+}
--- test/SQE.CSharp.UnitTests/SQEGrammarTests.cs
+++ test/SQE.CSharp.UnitTests/SQEGrammarTests.cs
@@ -1,17 +1,24 @@
 ﻿using Antlr4.Runtime;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace SQE.CSharp.UnitTests
 {
     [TestClass]
     public class SQEGrammarTests
     {
-        private SQEParser Setup(string input)
+        private SQEParser Setup(string input, EventHandler<ErrorEventArgs> errorEventHandler = null)
         {
             AntlrInputStream inputStream = new AntlrInputStream(input);
             var lexer = new SQELexer(inputStream);
             CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
             var parser = new SQEParser(commonTokenStream);
+            var errorListener = new ErrorListener();
+            if (errorEventHandler != null)
+            {
+                errorListener.Error += errorEventHandler;
+            }
+            parser.AddErrorListener(errorListener);
             var expressionContext = parser.expression();
             return parser;
         }
@@ -20,6 +27,9 @@ namespace SQE.CSharp.UnitTests
         [DataRow("PropertyName = 3")]
         [DataRow("PropertyName=3")]
         [DataRow("PropertyName= 3")]
+        [DataRow(" PropertyName = 3")]
+        [DataRow("PropertyName = 3 ")]
+        [DataRow(" PropertyName = 3 ")]
         public void Test_Isolated_Property_Equals_Number_Expression(string input)
         {
             var cts = (CommonTokenStream)Setup(input).InputStream;
@@ -35,6 +45,9 @@ namespace SQE.CSharp.UnitTests
         [DataRow("PropertyName = \"Text\"")]
         [DataRow("PropertyName=\"Text\"")]
         [DataRow("PropertyName= \"Text\"")]
+        [DataRow(" PropertyName= \"Text\"")]
+        [DataRow("PropertyName= \"Text\" ")]
+        [DataRow(" PropertyName= \"Text\" ")]
         public void Test_Isolated_Property_Equals_String_Expression(string input)
         {
             var cts = (CommonTokenStream)Setup(input).InputStream;
@@ -44,6 +57,50 @@ namespace SQE.CSharp.UnitTests
             Assert.AreEqual(SQELexer.PROPERTY, cts.Get(0).Type);
             Assert.AreEqual("=", cts.Get(1).Text);
             Assert.AreEqual(SQELexer.ESCAPEDSTRING, cts.Get(2).Type);
+        }
+
+        [DataTestMethod]
+        [DataRow("test PropertyName = \"Text\"")]
+        [DataRow("\"test PropertyName = \"Text\"")]
+        [DataRow("test\" PropertyName = \"Text\"")]
+        [DataRow("test \"PropertyName = \"Text\"")]
+        [DataRow("\"test\" PropertyName = \"Text\"")]
+        [DataRow("\"test\"PropertyName = \"Text\"")]
+        public void Test_Property_Equals_String_Expression_Wrong_Start(string input)
+        {
+            var throwsError = false;
+
+            var parser = Setup(input, (sender, args) =>
+            {
+                if (args.Error != null)
+                {
+                    throwsError = true;
+                }
+            });
+
+            Assert.IsTrue(throwsError);
+        }
+
+        [DataTestMethod]
+        [DataRow("PropertyName = \"Text\" test")]
+        [DataRow("PropertyName = \"Text\"\"test")]
+        [DataRow("PropertyName = \"Text\"test\" ")]
+        [DataRow("PropertyName = \"Text\" test \"")]
+        [DataRow("PropertyName = \"Text\" \"test\" ")]
+        [DataRow("PropertyName = \"Text\"\"test\"")]
+        public void Test_Property_Equals_String_Expression_Wrong_End(string input)
+        {
+            var throwsError = false;
+
+            var parser = Setup(input, (sender, args) =>
+            {
+                if (args.Error != null)
+                {
+                    throwsError = true;
+                }
+            });
+
+            Assert.IsTrue(throwsError);
         }
     }
 }
