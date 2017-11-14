4eeb2e438d53566d64844cac189f6e36f269ca27
David Morais Ferreira <david.moraisferreira@gmail.com>
2017-11-14 22:01:17 +0100

    Output some messages when building Antlr files

--- src/SQE.Grammar/build.ps1
+++ src/SQE.Grammar/build.ps1
@@ -4,16 +4,20 @@ param(
 	[String]$Package="SQE",
 	[String]$Output="../SQE.CSharp/Generated"
 )
+Write-Output "Start of build.ps1"
 
 if (-Not (Test-Path($AntlrJar)))
 {
+	Write-Output "Antlr runtime is missing. Jar File is being downloaded."
 	Invoke-WebRequest http://www.antlr.org/download/$AntlrJar -OutFile $AntlrJar
 }
 
 try
 {
+	Write-Output "Building Antlr files and generating C# classes."
 	java -jar $AntlrJar -Dlanguage=CSharp $Grammar -visitor -no-listener -package $Package -o $Output
 }
 catch {
 	Write-Error "Could not generate the C# files, please verify that you have java installed on your machine."
 }
+Write-Output "End of build.ps1"
