5d7c717db016dce8cc09a75df60db84f9cf6e528
Sebastian Lang <admin@sebastian-lang.eu>
2017-12-01 20:17:20 +0100

    [JSProject] Fix for IE11

--- test/SQE.JS.Test/wwwroot/lib/require.js
+++ test/SQE.JS.Test/wwwroot/lib/require.js
@@ -1,29 +1,39 @@
 //
-// This file is part of Honey Require
+// This file is part of Smoothie.
 //
-// Copyright (c) 2015 Torben Haase
+// Copyright (C) 2013-2015 Flowy Apps GmbH <hello@flowyapps.com>
 //
-// Honey Require is free software: you can redistribute it and/or modify it
-// under the terms of the MIT License (MIT).
+// Smoothie is free software: you can redistribute it and/or modify it under the
+// terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option) any
+// later version.
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-//
-// You should have received a copy of the MIT License along with Honey Require.
-// If not, see <https://opensource.org/licenses/MIT>.
+// Smoothie is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+// details.You should have received a copy of the GNU Lesser General Public
+// License along with Smoothie.  If not, see <http://www.gnu.org/licenses/>.
 //
 ////////////////////////////////////////////////////////////////////////////////
+
+// INFO Standalone require()
+//      This is a stripped down standalone version of Smoothie's require
+//      function. If you also like to have a 'bootloader', which gives you some
+//      nice hooks to execute code on different loading states of the document
+//      and keeps your JavaScript completely separate from your HTML, we
+//      recommend to load smoothie.js from the library's root directory.
 
 // NOTE The load parameter points to the function, which prepares the
 //      environment for each module and runs its code. Scroll down to the end of
 //      the file to see the function definition.
 (function (load) {
     'use strict';
+
+    var SmoothieError = function (message, fileName, lineNumber) {
+        this.name = "SmoothieError";
+        this.message = message;
+    }
+    SmoothieError.prototype = Object.create(Error.prototype);
 
     // NOTE Mozilla still sets the wrong fileName property for errors that occur
     //      inside an eval call (even with sourceURL). However, the stack
@@ -41,7 +51,7 @@
                 else {
                     var m = evt.error.stack.match(/^[^\n@]*@([^\n]+):\d+:\d+/);
                     if (m === null) {
-                        console.warn("Honey: unable to read file name from stack");
+                        console.warn("Smoothie: unable to read file name from stack");
                     }
                     else if (evt.error.fileName != m[1]) {
                         evt.preventDefault();
@@ -63,16 +73,18 @@
     //      anchor element as parser in that case. Thes breaks web worker support,
     //      but we don't care since these browsers also don't support web workers.
 
-    var parser = (function () {
+    var parser;
+
+    if (URL) {
         try {
-            return new URL(location.href);
+            parser = new URL(location.href);
         } catch (e) {
-            var p = Object.create(location);
-            // need to set writable, because WorkerLocation is read-only
-            Object.defineProperty(p, "href", { writable: true });
-            return p;
+            parser = document.createElement('A');
         }
-    })();
+    } else {
+        parser = document.createElement('A');
+    }
+
 
     // INFO Module cache
     //      Contains getter functions for the exports objects of all the loaded
@@ -81,17 +93,17 @@
     //      As long as a module has not been loaded the getter is either undefined
     //      or contains the module code as a function (in case the module has been
     //      pre-loaded in a bundle).
-    // WARN IE8 supports defineProperty only for DOM objects, therfore we use a
+
+    // NOTE IE8 supports defineProperty only for DOM objects, therfore we use a
     //      HTMLDivElement as cache in that case. This breaks web worker support,
     //      but we don't care since IE8 has no web workers at all.
-
     try {
         var cache = new Object();
         Object.defineProperty(cache, "foo", { 'value': "bar", 'configurable': true });
         delete cache.foo;
     }
     catch (e) {
-        console.warn("Honey: falling back to DOM workaround for cache object (" + e + ")");
+        console.warn("Smoothie: Falling back to DOM workaround for defineProperty: " + e);
         cache = document.createElement('DIV');
     }
 
@@ -100,33 +112,36 @@
     //      pending AJAX load events for the same url might be executed before
     //      the synchronous onLoad is called. This should be no problem, but in
     //      Chrome the responseText of the sneaked in load events will be empty.
-    //      Therefore we have to lock the loading while executing send().
+    //      Therefore we have to lock the loading while executing send().   
 
     var lock = new Object();
 
-    // INFO Honey options
-    //      The values can be set by defining a object called Honey. The
-    //      Honey object has to be defined before this script here is loaded
-    //      and changing the values in the Honey object will have no effect
+    // INFO Smoothie options
+    //      The values can be set by defining a object called Smoothie. The
+    //      Smoothie object has to be defined before this script here is loaded
+    //      and changing the values in the Smoothie object will have no effect
     //      afterwards!
 
-    var requirePath = self.Honey && self.Honey.requirePath !== undefined ? self.Honey.requirePath.slice(0) : ['./'];
-    var requireCompiler = self.Honey && self.Honey.requireCompiler !== undefined ? self.Honey.requireCompiler : null;
+    var requirePath = self.Smoothie && self.Smoothie.requirePath !== undefined ? self.Smoothie.requirePath.slice(0) : ['./'];
+    var requireCompiler = self.Smoothie && self.Smoothie.requireCompiler !== undefined ? self.Smoothie.requireCompiler : null;
 
     // NOTE Parse module root paths
     var base = [location.origin, location.href.substr(0, location.href.lastIndexOf("/") + 1)];
     for (var i = 0; i < requirePath.length; i++) {
-        parser.href = (requirePath[i][0] == "." ? base[1] : base[0]) + requirePath[i];
+        if (!/^(?:\w+:)?\/\//.test(requirePath[i]))
+            parser.href = (requirePath[i][0] == "." ? base[1] : base[0]) + requirePath[i];
+        else
+            parser.href = requirePath[i];
         requirePath[i] = parser.href;
     }
 
     // NOTE Add preloaded modules to cache
-    for (var id in (self.Honey && self.Honey.requirePreloaded))
-        cache['$' + resolve(id).id] = self.Honey.requirePreloaded[id].toString();
+    for (var id in (self.Smoothie && self.Smoothie.requirePreloaded))
+        cache['$' + resolve(id).id] = self.Smoothie.requirePreloaded[id].toString();
 
     // NOTE Add module overrides to cache
-    for (var id in (self.Honey && self.Honey.requireOverrides))
-        cache['$' + resolve(id).id] = self.Honey.requireOverrides[id];
+    for (id in (self.Smoothie && self.Smoothie.requireOverrides))
+        cache['$' + resolve(id).id] = self.Smoothie.requireOverrides[id];
 
     // INFO Module getter
     //      Takes a module identifier, resolves it and gets the module code via an
@@ -160,42 +175,43 @@
         if (cache[cacheid]) {
             if (typeof cache[cacheid] === 'string')
                 load(descriptor, cache, pwd, cache[cacheid]);
-            // NOTE The callback should always be called asynchronously to ensure
-            //      that a cached call won't differ from an uncached one.
-            callback && setTimeout(function () { callback(cache[cacheid]) }, 0);
+            if (callback)
+                // NOTE The callback should always be called asynchronously to ensure
+                //      that a cached call won't differ from an uncached one.
+                setTimeout(function () { callback(cache[cacheid]); }, 0);
             return cache[cacheid];
         }
 
         var request = new XMLHttpRequest();
 
-        // NOTE IE8 doesn't support the onload event, therefore we use
-        //      onreadystatechange as a fallback here. However, onreadystatechange
-        //      shouldn't be used for all browsers, since at least mobile Safari
-        //      seems to have an issue where onreadystatechange is called twice for
-        //      readyState 4.
-        callback && (request[request.onload === null ? 'onload' : 'onreadystatechange'] = onLoad);
+        if (callback)
+            request.onload = onLoad;
         request.open('GET', descriptor.uri, !!callback);
+        // NOTE Locking is required to prevent some browsers from running onLoad during load
         lock[cacheid] = lock[cacheid]++ || 1;
         request.send();
         lock[cacheid]--;
-        !callback && onLoad();
+        if (!callback)
+            onLoad();
         return cache[cacheid];
 
         function onLoad() {
-            if (request.readyState != 4)
-                return;
-            if (request.status != 200)
-                throw new Error("Honey: unable to load " + descriptor.id + " (" + request.status + " " + request.statusText + ")");
             if (lock[cacheid]) {
-                console.warn("Honey: module locked: " + descriptor.id);
-                callback && setTimeout(onLoad, 0);
-                return;
+                console.warn("Smoothie: module locked: " + descriptor.id);
+                setTimeout(onLoad, 0);
             }
-            if (!cache[cacheid]) {
-                var source = compiler ? compiler(request.responseText) : request.responseText;
-                load(descriptor, cache, pwd, 'function(){\n' + source + '\n}');
+            else {
+                if (request.readyState != 4)
+                    return;
+                if (request.status != 200)
+                    throw new SmoothieError("unable to load " + descriptor.id + " (" + request.status + " " + request.statusText + ")");
+                if (!cache[cacheid]) {
+                    var source = compiler ? compiler(request.responseText) : request.responseText;
+                    load(descriptor, cache, pwd, 'function(){\n' + source + '\n}');
+                }
+                if (callback)
+                    callback(cache[cacheid]);
             }
-            callback && callback(cache[cacheid]);
         }
     }
 
@@ -213,15 +229,15 @@
         parser.href = (m[2] ? root + p[2] + m[2] + '/' : root) + m[3] + (m[4] ? m[4] : 'index');
         var uri = parser.href + (m[5] ? m[5] : '.js');
         if (uri.substr(0, root.length) != root)
-            throw new Error("Honey: relative identifier outside of module root");
+            throw new SmoothieError("relative identifier outside of module root");
         var id = (m[1] ? m[1] + ":" : "0:") + parser.href.substr(root.length);
         return { 'id': id, 'uri': uri };
     }
 
-    // INFO Exporting require to global scope
+    // NOTE Export require to global scope
 
     if (self.require !== undefined)
-        throw new Error("Honey: '\'require\' already defined in global scope");
+        throw new SmoothieError("'require' already defined in global scope");
 
     try {
         Object.defineProperty(self, 'require', { 'value': require });
@@ -260,7 +276,7 @@
         eval(arguments[3]);
         // NOTE Store module code in the cache if the loaded file is a bundle
         if (typeof module.id !== 'string')
-            for (id in module)
+            for (var id in module)
                 arguments[1]['$' + require.resolve(id).id] = module[id].toString();
         arguments[2].shift();
     }
