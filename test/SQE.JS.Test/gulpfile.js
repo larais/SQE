261e3face4c0de4a38cf0b66cbe932ab033a2c8f
David Morais Ferreira <david.moraisferreira@gmail.com>
2018-09-17 23:15:19 +0200

    Removed semantic-ui and JQuery from npm, now loaded using a CDN

--- test/SQE.JS.Test/gulpfile.js
+++ test/SQE.JS.Test/gulpfile.js
@@ -1,23 +1,17 @@
 ﻿var gulp = require('gulp');
 
-gulp.task("copy-antlr", function () {
+gulp.task("copy-antlr", function (done) {
     gulp.src([
         "node_modules/antlr4/**/*"
-    ])
-        .pipe(gulp.dest("wwwroot/antlr4"));
+    ]).pipe(gulp.dest("wwwroot/antlr4"));
+    done();
 });
 
-gulp.task('release', gulp.series(["copy-antlr"], function () {
-    gulp.src([
-        "node_modules/jquery/dist/jquery.min.js",
-        "semantic/dist/semantic.min.css",
-        "semantic/dist/semantic.min.js"
-    ])
-        .pipe(gulp.dest("wwwroot/lib"));
-}));
+
+gulp.task('release', gulp.series(["copy-antlr"]));
 
 
-gulp.task("publish-docs", function () {
-    gulp.src("wwwroot/**/*")
-        .pipe(gulp.dest("../../docs"));
+gulp.task("publish-docs", function (done) {
+    gulp.src("wwwroot/**/*").pipe(gulp.dest("../../docs"));
+    done();
 });
